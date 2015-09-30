dca3dfd3d4125670373906949e695807854c89bb
Patrick Baselier <patrick.baselier@gmail.com>
2015-09-30 07:42:05 +0200

    Add GET request for single product to Mirage

--- frontend/app/mirage/config.js
+++ frontend/app/mirage/config.js
@@ -1,9 +1,20 @@
 export default function() {
-  this.get('/products', function(db, request) {
+  this.get('/products', function(db) {
     return {
       data: db.products.map(attrs => (
         {type: 'products', id: attrs.id, attributes: attrs }
       ))
     };
+  });
+  this.get('/products/:id', function(db, request) {
+      var id = request.params.id;
+
+      return {
+        data: {
+          type: 'products',
+           id: id,
+           attributes: db.products.find(id)
+        }
+      };
   });
 }
