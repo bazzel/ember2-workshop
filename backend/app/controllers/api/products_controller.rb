551ec0704e63ee3d46333a76177c444a1c951a7f
Patrick Baselier <patrick.baselier@gmail.com>
2015-10-12 08:52:19 +0200

    Implement chapter 6

--- backend/app/controllers/api/products_controller.rb
+++ backend/app/controllers/api/products_controller.rb
@@ -1,7 +1,10 @@
 module Api
   class ProductsController < JSONAPI::ResourceController
-    #def index
-      #render json: Product.all
-    #end
+    before_action :delay
+
+    private
+    def delay
+      sleep 1
+    end
   end
 end
